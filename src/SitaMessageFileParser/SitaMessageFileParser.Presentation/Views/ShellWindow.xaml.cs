c2fece6c7d1d86d7271fd8ad0617c606a8073dea
Mohamed Alalaiwat <moh9277830@hotmail.com>
2018-02-12 10:45:45 +0300

    ShellWindow update

--- src/SitaMessageFileParser/SitaMessageFileParser.Presentation/Views/ShellWindow.xaml.cs
+++ src/SitaMessageFileParser/SitaMessageFileParser.Presentation/Views/ShellWindow.xaml.cs
@@ -24,5 +24,25 @@ namespace SitaMessageFileParser.Presentation.Views
         {
             InitializeComponent();
         }
+
+        public double VirtualScreenWidth => SystemParameters.VirtualScreenWidth;
+
+        public double VirtualScreenHeight => SystemParameters.VirtualScreenHeight;
+
+        public bool IsMaximized
+        {
+            get { return WindowState == WindowState.Maximized; }
+            set
+            {
+                if(value)
+                {
+                    WindowState = WindowState.Maximized;
+                }
+                else if(WindowState == WindowState.Maximized)
+                {
+                    WindowState = WindowState.Normal;
+                }
+            }
+        }
     }
 }
